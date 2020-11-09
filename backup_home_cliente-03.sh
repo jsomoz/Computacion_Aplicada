5c0a277bca0cff3d215b2818e78cbd23cb6dcae7
jsomoz <74158993+jsomoz@users.noreply.github.com>
2020-11-08 22:40:38 -0300

    Create backup_home_cliente-03.sh

---
+++ backup_home_cliente-03.sh
@@ -0,0 +1,10 @@
+ping -q -c 1 192.168.20.3
+if [ $? = 0 ]
+then
+{ DATE = $(date +%Y-%m-%d_%H:%M:%S)
+  rsync -avzh user_fileserver@192.168.20.3:/home/jsomoz media/disco_backups/disco_backups/backup/$DATE.tar.gz
+  echo Backup de la carpeta realizada.
+}
+else
+echo No se pudo realizar el backup, no hay conexion.
+fi
